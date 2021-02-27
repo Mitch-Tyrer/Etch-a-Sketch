dcb3765bd95809863cb71fd3a213188ac559eacf
Mitch Tyrer <mltcinematography@gmail.com>
2021-02-27 11:32:06 -0700

    updated css to lock grid in the center, reset btn logic implemented

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 // Select container div of html
-const container = document.querySelector('#grid-container');
+const container = document.querySelector('.container');
+const grid = document.querySelector('#grid-container');
+let resetBtn = document.createElement("input");
+resetBtn.type = "button"; resetBtn.value = "Reset"; resetBtn.classList = "reset-btn"
+container.insertBefore(resetBtn, container.childNodes[0]);
+
 // Create a 16X16 grid of div elements
 function createGrid(x) {
+    grid.setAttribute('style', `grid-template-columns: repeat(${x}, 1fr);`)
     for (var i = 0; i < (x ** 2); i++){
         let cell = document.createElement('div');
         cell.addEventListener('mouseover', function(e){
             e.target.style.background = 'black';
         });
-        container.appendChild(cell).setAttribute('class', 'cell')
+        grid.appendChild(cell).setAttribute('class', 'cell')
     }
 }
+
+// Reset the grid function
+function resetGrid() {
+    let grid = document.querySelectorAll('.cell');
+    grid.forEach(cell => cell.remove());
+    let newGrid = prompt("How many Squares?");
+    if (newGrid <= 100) {
+        createGrid(newGrid);
+    } else {
+        newGrid = prompt("Grid cannot be larger than 100");
+        createGrid(newGrid);
+    }
+}
+
+// Event listeners
+document.querySelector(".reset-btn").addEventListener('click', resetGrid);
 
 createGrid(16);
